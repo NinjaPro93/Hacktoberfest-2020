aba388f1a2a4d29f6e341a714fbc6a92f1c79e05
NinjaPro93 <68875016+NinjaPro93@users.noreply.github.com>
2020-10-19 14:23:32 -0300

    Update exc 8.py

--- exc 8.py
+++ exc 8.py
@@ -3,3 +3,20 @@ exc 8
 Preencher uma lista com 5 números e a medida que for digitado o número, calcular o cubo e mostrar em outra lista. 
 Mostrar as duas listas.
 '''
+lista11 = []
+cubos = []
+
+for i in range(5):
+    x = float(input("Digite um número:"))
+    lista11.append(x)
+
+for i in range(5):
+    y = lista11[i]**3
+    cubos.append(y)
+
+print('='*30)
+
+print('''
+Nºs originais: {}
+Elevados ao cubo: {}
+'''.format(lista11[:],cubos[:]))
