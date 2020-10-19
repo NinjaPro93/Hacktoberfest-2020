ae6b26ce47c4eccaf4da575b21cd359b8a1b9897
NinjaPro93 <68875016+NinjaPro93@users.noreply.github.com>
2020-10-19 14:14:40 -0300

    Update exc 7.py

--- exc 7.py
+++ exc 7.py
@@ -3,3 +3,20 @@ exc 7
 Preencher uma lista com 5 números e guardar o cubo dos números em outro lista. 
 Mostrar as duas listas.
 '''
+lista11 = []
+cubos = []
+
+for i in range(5):
+    x = float(input("Digite um número:"))
+    lista11.append(x)
+
+for i in range(5):
+    y = lista11[i]**3
+    cubos.append(y)
+
+print('='*30)
+
+print('''
+Nºs originais: {}
+Elevados ao cubo: {}
+'''.format(lista11[:],cubos[:]))
