688cdfaa4440706214158c020b23b0c7a8735a3b
Matheus Cavalcanti <56284773+D4NNONY@users.noreply.github.com>
2020-10-19 14:28:09 -0300

    Update exc 8.py

--- exc 8.py
+++ exc 8.py
@@ -1,5 +1,4 @@
 '''
 exc 8
-Preencher uma lista com 5 números e a medida que for digitado o número, calcular o cubo e mostrar em outra lista. 
-Mostrar as duas listas.
+Preencher  uma  lista  com  3  nomes  e mostrar  quantas  letras A  e  E  tem  nos  3 nomes.
 '''
