4a9e048bc22f9f1fb70bf78692bdcd0f9f100fd4
NinjaPro93 <68875016+NinjaPro93@users.noreply.github.com>
2020-10-19 14:24:34 -0300

    Update exc 9.py

--- exc 9.py
+++ exc 9.py
@@ -3,3 +3,28 @@ exc 99
 Neste exercício temos duas listas com 5 posições (0 a 4). Em cada lista entraremos com cinco números. 
 Mostrar os números e depois somar números que pertençam a mesma posição ou seja: [0]+[0], [1]+[1]
 '''
+lista12 = []
+lista13 = []
+lista14 = []
+
+for i in range(5):
+    x = float(input("Digite um número para a 1º lista:"))
+    lista12.append(x)
+
+for i in range(5):
+    y = float(input("Digite um número para a 2º lista:"))
+    lista13.append(y)
+
+for i in range(5):
+    z = (lista12[i])+(lista13[i])
+    lista14.append(z)
+
+print('='*30)
+
+print('''
+Listas originais:
+{}
+{}
+Soma das listas:
+{}
+'''.format(lista12[:],lista13[:],lista14[:]))
