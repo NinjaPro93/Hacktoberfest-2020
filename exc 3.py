7f77d0001567c8f684773af48a952f29c844417e
NinjaPro93 <68875016+NinjaPro93@users.noreply.github.com>
2020-10-19 14:39:54 -0300

    Update exc 3.py

--- exc 3.py
+++ exc 3.py
@@ -4,3 +4,4 @@ Preencher uma lista com números inteiros (8 unidades) solicitar um número do t
 Pesquisar se esse número existe no vetor, usando o comando for. 
 Se existir, imprimir em qual posição do vetor. Se não existir, imprimir MSG que não existe.
 '''
+
